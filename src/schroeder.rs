8e54a4540d3441718d6f6ac85a68e2668812b7eb
mirrorganger <cesargufer@gmail.com>
2025-01-19 20:26:26 +0100

    process added

--- src/schroeder.rs
+++ src/schroeder.rs
@@ -1,5 +1,6 @@
 use crate::all_pass::AllPass;
 use crate::comb::Comb;
+use crate::processor::AudioProcessor;
 
 const NUM_COMBS: usize = 4;
 const NUM_APF: usize = 2;
@@ -30,7 +31,7 @@ impl Schroeder {
         let comb_delay_length = get_length_in_samples(COMB_MAX_DELAY_MS, sample_rate) as usize;
         let apf_delay_length = get_length_in_samples(COMB_MAX_DELAY_MS, sample_rate) as usize;
 
-        let schroeder = Schroeder {
+        Schroeder {
             combs: [
                 (Comb::new(comb_delay_length, true), COMB_DELAYS_MS[0]),
                 (Comb::new(comb_delay_length, true), COMB_DELAYS_MS[1]),
@@ -43,8 +44,7 @@ impl Schroeder {
             ],
             sample_rate,
             dry_wet_mix: 0.5,
-        };
-        schroeder
+        }
     }
 
     pub fn prepare(&mut self, sample_rate: f64, rt60_ms: f64) {
@@ -59,16 +59,52 @@ impl Schroeder {
             all_pass.prepare(delay_samples, 0.5 * (2f64).sqrt());
         }
     }
+
+    pub fn set_dampening(&mut self, dampening: f64) {
+        for (comb, _) in self.combs.iter_mut() {
+            comb.set_dampening(dampening);
+        }
+    }
+
+    pub fn set_dry_wet_mix(&mut self, dry_wet_mix: f64) {
+        self.dry_wet_mix = dry_wet_mix;
+    }
+
+    pub fn update_reverb_time(&mut self, rt60_ms: f64) {
+        for (comb, delay_ms) in self.combs.iter_mut() {
+            comb.set_gain(get_gain_from_rt60(*delay_ms, rt60_ms))
+        }
+    }
+}
+
+impl AudioProcessor<f64> for Schroeder {
+    fn process(&mut self, input: f64) -> f64 {
+        let mut out: f64 = 0.0;
+        for (index, (combs, _)) in self.combs.iter_mut().enumerate() {
+            let mut comb_out = combs.process(input);
+            if index % 2 == 0 {
+                comb_out *= -1.0
+            };
+            out += comb_out;
+        }
+        for (all_pass, _) in self.all_passes.iter_mut() {
+            out = all_pass.process(out);
+        }
+        out * self.dry_wet_mix + input * (1.0 - self.dry_wet_mix)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_creation(){
+    fn test_creation() {
+        use super::*;
         let sample_rate = 44100.0;
         let rt60_ms = 20.0;
-        let mut uut = super::Schroeder::new(sample_rate);
+        let mut uut = Schroeder::new(sample_rate);
         uut.prepare(sample_rate, rt60_ms);
+        uut.set_dampening(0.5);
+        uut.set_dry_wet_mix(0.6);
+        let _out = uut.process(0.3);
     }
 }
-
