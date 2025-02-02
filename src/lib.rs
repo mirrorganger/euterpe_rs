90f85cf56c6358bc12d016142d2718f4fccada81
mirrorganger <cesargufer@gmail.com>
2025-02-02 19:06:31 +0100

    remove schroeder app

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
-mod all_pass;
-mod comb;
-mod delay_line;
-mod onepole_lpf;
+pub mod all_pass;
+pub mod comb;
+pub mod delay_line;
+pub mod onepole_lpf;
 pub mod processor;
-pub mod schroeder;
+// pub mod schroeder;
