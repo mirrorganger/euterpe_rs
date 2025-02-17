b39135f228b76ce6f631f07a6eef3c9fc946aacf
mirrorganger <cesargufer@gmail.com>
2025-02-18 00:21:07 +0100

    Waveform type for modulated delay new

--- src/mod_delay.rs
+++ src/mod_delay.rs
@@ -13,14 +13,14 @@ pub struct ModulatedDelay {
 const MAX_DELAY_MS: f32 = 100.0;
 
 impl ModulatedDelay {
-    pub fn new(delay_ms: f32, mod_freq: f32, sample_rate_hz: f32) -> Self {
+    pub fn new(delay_ms: f32, mod_freq: f32, mod_wave: WaveformType, sample_rate_hz: f32) -> Self {
         ModulatedDelay {
             delay_line: DelayLine::new((MAX_DELAY_MS * sample_rate_hz / 1000.0) as usize), // 100 ms delay
-            lfo: Lfo::new(WaveformType::Triangle, mod_freq, sample_rate_hz),
+            lfo: Lfo::new(mod_wave, mod_freq, sample_rate_hz),
             lfo_depth: 1.0,
             mod_width_ms: 0.1 * delay_ms,
             delay_ms,
-            sample_rate : sample_rate_hz,
+            sample_rate: sample_rate_hz,
         }
     }
 
@@ -60,10 +60,11 @@ mod tests {
         let mod_freq: f32 = 100.0;
         let sample_rate: f32 = 1000.0; // 1ms -> 1sample
         let delay_samples = (delay_ms) as usize;
-        let mod_samples = 2 * (0.1 * delay_samples as f32) as usize;
+        let mod_samples = 2 * (0.1 * delay_samples as f32) as usize; // bipolar, +1, -1 sample
 
-        let mut mod_delay = ModulatedDelay::new(delay_ms, mod_freq, sample_rate);
-        let samples: Vec<f64> = (0..delay_samples+mod_samples).map(|x| x as f64).collect();
+        let mut mod_delay =
+            ModulatedDelay::new(delay_ms, mod_freq, WaveformType::Triangle, sample_rate);
+        let samples: Vec<f64> = (0..delay_samples + mod_samples).map(|x| x as f64).collect();
         let mut out_samples: Vec<f64> = Vec::new();
 
         for &sample in samples.iter() {
@@ -76,7 +77,6 @@ mod tests {
 
         let lower_bound = samples[0];
         let upper_bound = samples[mod_samples];
-        assert!(is_bounded_buffer(&out_samples,lower_bound, upper_bound));
-
+        assert!(is_bounded_buffer(&out_samples, lower_bound, upper_bound));
     }
 }
